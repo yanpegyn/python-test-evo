b48d645ce398f8a5d55e307fec87addf685bc611
Yan Gustavo Pegyn Silva <46832714+yanpegyn@users.noreply.github.com>
2024-01-16 15:10:55 -0300

    fix and doc

--- src/app/obj_relacionado/simple_object.py
+++ src/app/obj_relacionado/simple_object.py
@@ -3,6 +3,7 @@
 class SimpleObject:
     def __init__(self, valor_a):
         self.a = valor_a
+        self._b = "Private"
 
     @property
     def a(self):
@@ -14,3 +15,5 @@ class SimpleObject:
             raise ValueError("value must be less than zero")
         self._a = value
 
+    def __b(self):
+        return self._b
